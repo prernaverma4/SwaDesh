3911b68cdabc520e8d1ebd413db1572a83018895
prernaverma4 <160102100+prernaverma4@users.noreply.github.com>
2026-05-01 13:12:42 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      root: process.cwd(),
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
